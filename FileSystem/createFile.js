ea4e1bc64830ed8b63c872971ff58e9ea6555c10
PiyushJiwane <jiwanepiyush@gmail.com>
2023-08-08 10:59:02 +0530

    writeFileSync() function will create a new file and add data in the file.

---
+++ FileSystem/createFile.js
@@ -0,0 +1,5 @@
+const fs=require("fs")
+
+// Comment : First it will create a file and write the data inside a file as data.txt file is not present
+fs.writeFileSync("../Data/data.txt", "This is a first file")
+
