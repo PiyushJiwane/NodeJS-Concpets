aee1ca1f87827d70be340849d33aaa85c0d7bbf5
PiyushJiwane <jiwanepiyush@gmail.com>
2023-08-08 22:13:18 +0530

    updated : introduced cpus(), homefir(), uptime(), userInfo(), version() in os module.

--- OperatingSystem/os_module.js
+++ OperatingSystem/os_module.js
@@ -21,3 +21,20 @@ console.log(os.tmpdir());
 
 // Comment : Returns the operating system name as returned by uname(3). For example, it returns 'Linux' on Linux, 'Darwin' on macOS, and 'Windows_NT' on Windows.
 console.log(os.type());
+
+// Comment : Returns an array of objects containing information about each logical CPU core. The array will be empty if no CPU information is available, such as if the/proc file system is unavailable.
+console.log(os.cpus());
+
+// Comment : Returns the string path of the current user's home directory.
+console.log(os.homedir());
+
+// Comment : Returns the system uptime in number of seconds.
+console.log(os.uptime());
+console.log(os.uptime()/60/60);
+
+
+// Comment : Returns information about the currently effective user. On POSIX platforms, this is typically a subset of the password file. The returned object includes the username, uid, gid, shell, and homedir. On Windows, the uid andgid fields are -1, and shell is null.
+console.log(os.userInfo());
+
+// Comment : Returns a string identifying the kernel version.
+console.log(os.version());
