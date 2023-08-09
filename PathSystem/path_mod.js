69b7189e8899ccc327ec847226f6eca4e9611f88
PiyushJiwane <jiwanepiyush@gmail.com>
2023-08-09 12:21:44 +0530

    updated : In this file we introduce __dirname, __filename properties alogn with the format() and join()

--- PathSystem/path_mod.js
+++ PathSystem/path_mod.js
@@ -1,5 +1,11 @@
 const path=require("path")
 
+// Comment : It returns the current working directory path
+console.log(__dirname);
+
+// Comment : It returns the current workign file path
+console.log(__filename);
+
 // Comment : Return the directory name of a path. Similar to the Unix dirname command.
 console.log(path.dirname("/home/rootadmin/ProjectPractice/NodeJS-Concpets/PathSystem/path_mod.js"));
 
@@ -19,5 +25,17 @@ console.log(path.parse("/home/rootadmin/ProjectPractice/NodeJS-Concpets/PathSyst
 //     ext: '.js',
 //     name: 'path_mod'
 // }
+
+// Comment : Returns a path string from an object - the opposite of parse()
+let formatted=path.format({
+    root:"/home",
+    dir:"/usr/dir",
+    base:"index.js"
+})
+console.log(formatted);
+
+// Comment : Join all arguments together and normalize the resulting path.
+console.log(path.join(__dirname+"/path_mod.js"));
+
 
 
