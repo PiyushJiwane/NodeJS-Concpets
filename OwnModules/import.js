08b1447e6ca6610708da7a4568a359efe5b8a15e
PiyushJiwane <jiwanepiyush@gmail.com>
2023-08-10 10:58:41 +0530

    import a variable in and demonstarte how to use it in import.js file

--- OwnModules/import.js
+++ OwnModules/import.js
@@ -1,3 +1,6 @@
 const add=require("../OwnModules/export")
 
-console.log(add(5,10));
+// console.log(add(5,10)); // Here add() is a function because we are exporting a function from export.js file
+console.log(add); // Here add is a varible because we are exporting a variable from export.js file
+
+// NOTE : whenever we export any variable or function, that we can import with in a file with any name.
