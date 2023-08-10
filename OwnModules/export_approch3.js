024699249039b6a153a3a5cf8516e4a29f7dd07c
PiyushJiwane <jiwanepiyush@gmail.com>
2023-08-10 11:21:09 +0530

    introduce approch3 - by using object destructuring.

---
+++ OwnModules/export_approch3.js
@@ -0,0 +1,12 @@
+function add(n1,n2){
+    return n1+n2
+}
+
+function sub(n1,n2){
+    return n1-n2
+}
+
+const name="piyush"
+
+// Approach 3 :
+module.exports={add,sub}
---
+++ OwnModules/import_approch3.js
@@ -0,0 +1,5 @@
+const {add,sub}=require("./export_approch3")
+
+console.log(add(5,10));
+
+console.log(sub(5,10));
