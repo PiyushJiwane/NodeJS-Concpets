8b85d1be36bca81f7be217c750c7404ad1a0c195
PiyushJiwane <jiwanepiyush@gmail.com>
2023-08-10 11:06:51 +0530

    updated : file name

---
+++ OwnModules/export_approch1.js
@@ -0,0 +1,10 @@
+// Every file in node js is a module in itself, and default scope of funcction or a variable is private in .js file. So we cannot directly call a function or variable ffrom another file.
+
+function add(n1,n2){
+    return n1+n2
+}
+
+const name="piyush"
+
+// Approach 1 :
+module.exports=name
