d403e867c0c8ff8af0220f92ce123ee55e421338
PiyushJiwane <jiwanepiyush@gmail.com>
2023-08-10 11:12:48 +0530

    demonstrate how to export and import multiple function.

---
+++ OwnModules/export_approch2.js
@@ -0,0 +1,13 @@
+function add(n1,n2){
+    return n1+n2
+}
+
+function sub(n1,n2){
+    return n1-n2
+}
+
+const name="piyush"
+
+// Approach 2 :
+module.exports.add=add
+module.exports.sub=sub
---
+++ OwnModules/import_approch2.js
@@ -0,0 +1,7 @@
+const oprt=require("./export_approch2")
+
+console.log(oprt);
+
+console.log(oprt.add(5,10));
+
+console.log(oprt.sub(5,10));
